5271fa82c0c334cde39ec53ceb177facd61a2fb1
Yashkumar Nimje <yashkumarnimje@Yashkumars-MacBook-Pro.local>
2025-05-27 16:26:15 +0530

    Navbar changes and new services added

--- src/app/services/page.js
+++ src/app/services/page.js
@@ -1,9 +1,65 @@
-import React from 'react'
+"use client";
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
 
 const Services = () => {
   return (
-    <div>Services</div>
-  )
-}
+    <div>
+      <div className="text-center mt-20 mb-10 lg:mb-[15em]">
+        <h1 className="text-[#002249] text-6xl font-extrabold tracking-tight lg:text-7xl lg:font-extrabold">
+          Services
+        </h1>
+        <h2 className="text-[#69A1BB] text-2xl font-semibold mt-4">
+          <Link href="/" className="mr-2">
+            Home
+          </Link>
+          <span>|</span>
+          <Link href="/services" className="ml-2">
+            Services
+          </Link>
+        </h2>
+      </div>
+      <div className="flex flex-col items-center justify-center mt-10 mb-[8em] lg:grid lg:grid-cols-3 lg:grid-rows-2 lg:gap-8  lg:w-[80%] lg:mx-auto">
+        <div className="relative w-[330px] h-[267px] mb-8">
+          <Image src="/ser1.webp" alt="img1" fill className="object-cover" />
+          <div className="absolute bottom-0 w-full h-1/4 hover:h-[60%] bg-[rgba(0,34,73,0.7)] flex items-center justify-center text-white font-extrabold text-lg transition-all duration-300 ease-in-out">
+            UI/UX Design
+          </div>
+        </div>
+        <div className="relative w-[330px] h-[267px] mb-8">
+          <Image src="/ser2.webp" alt="img2" fill className="object-cover" />
+          <div className="absolute bottom-0 w-full h-1/4 hover:h-[60%] bg-[rgba(0,34,73,0.7)] flex items-center justify-center text-white font-extrabold text-lg transition-all duration-300 ease-in-out">
+            IT Consultancy
+          </div>
+        </div>
+        <div className="relative w-[330px] h-[267px] mb-8">
+          <Image src="/ser3.webp" alt="img3" fill className="object-cover" />
+          <div className="absolute bottom-0 w-full h-1/4 hover:h-[60%] bg-[rgba(0,34,73,0.7)] flex items-center justify-center text-white font-extrabold text-lg transition-all duration-300 ease-in-out">
+            Technology Prof.
+          </div>
+        </div>
+        <div className="relative w-[330px] h-[267px] mb-8">
+          <Image src="/ser4.webp" alt="img4" fill className="object-cover" />
+          <div className="absolute bottom-0 w-full h-1/4 hover:h-[60%] bg-[rgba(0,34,73,0.7)] flex items-center justify-center text-white font-extrabold text-lg transition-all duration-300 ease-in-out">
+            Web Development
+          </div>
+        </div>
+        <div className="relative w-[330px] h-[267px] mb-8">
+          <Image src="/ser5.webp" alt="img5" fill className="object-cover" />
+          <div className="absolute bottom-0 w-full h-1/4 hover:h-[60%] bg-[rgba(0,34,73,0.7)] flex items-center justify-center text-white font-extrabold text-lg transition-all duration-300 ease-in-out">
+            Web Development
+          </div>
+        </div>
+        <div className="relative w-[330px] h-[267px] mb-8">
+          <Image src="/ser6.webp" alt="img6" fill className="object-cover" />
+          <div className="absolute bottom-0 w-full h-1/4 hover:h-[60%] bg-[rgba(0,34,73,0.7)] flex items-center justify-center text-white font-extrabold text-lg transition-all duration-300 ease-in-out">
+            Game Design
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Services
+export default Services;
--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -4,8 +4,8 @@ import Image from "next/image";
 const Footer = () => {
   return (
     <div className="footer text-white bg-[#00152e] w-full">
-      <div className="upperpart bg-[#00152e] flex justify-start gap-[7em] mx-auto py-[6em] w-[80%] border-b-[0.3px] border-[#616161]">
-        <div className="bg-[#00152e] w-[32%]">
+      <div className="upperpart bg-[#00152e] flex flex-col lg:flex-row justify-start lg:gap-[7em] gap-[3.5em] mx-auto lg:py-[6em] py-[5em] w-[80%] lg:w-auto border-b-[0.3px] border-[#616161]">
+        <div className="bg-[#00152e] lg:w-[32%]">
           <Image
             src="/footer.webp"
             alt="desc"
@@ -19,7 +19,7 @@ const Footer = () => {
             that we bring to our community onferences, books, and our membership
             added to the online magazine.
           </p>
-          <p className="bg-[#00152e] text-[#c2d1e2] tracking-wide leading-7 w-[90%]">
+          <p className="bg-[#00152e] text-[#c2d1e2] tracking-wide leading-7 lg:w-[90%]">
             One thing that hasn’t changed is that we’re a small team — with most
             of us not working
           </p>
@@ -29,55 +29,55 @@ const Footer = () => {
           <div className="bg-[#00152e] text-[#c2d1e2] flex flex-row gap-12 ">
             <div className="bg-[#00152e]">
               <ul className="list-none">
-                <li className="bg-[#00152e] text-sm tracking-wider cursor-pointer">
-                  <i className="fa-solid fa-chevron-right bg-[#00152e] text-xs pr-2 pb-8"></i>
+                <li className="bg-[#00152e] text-sm tracking-wider cursor-pointer w-[90px]">
+                  <i className="fa-solid fa-chevron-right bg-[#00152e] text-xs mr-2 pb-8"></i>
                   Home
                 </li>
-                <li className="bg-[#00152e] text-sm tracking-wider cursor-pointer">
-                  <i className="fa-solid fa-chevron-right bg-[#00152e] text-xs pr-2 pb-8"></i>
+                <li className="bg-[#00152e] text-sm tracking-wider cursor-pointer w-[90px]">
+                  <i className="fa-solid fa-chevron-right bg-[#00152e] text-xs mr-2 pb-8"></i>
                   Services
                 </li>
-                <li className="bg-[#00152e] text-sm tracking-wider cursor-pointer">
-                  <i className="fa-solid fa-chevron-right bg-[#00152e] text-xs pr-2 pb-8"></i>
+                <li className="bg-[#00152e] text-sm tracking-wider cursor-pointer w-[90px]">
+                  <i className="fa-solid fa-chevron-right bg-[#00152e] text-xs mr-2 pb-8"></i>
                   About
                 </li>
-                <li className="bg-[#00152e] text-sm tracking-wider cursor-pointer">
-                  <i className="fa-solid fa-chevron-right bg-[#00152e] text-xs pr-2 pb-8"></i>
+                <li className="bg-[#00152e] text-sm tracking-wider cursor-pointer w-[90px]">
+                  <i className="fa-solid fa-chevron-right bg-[#00152e] text-xs mr-2 pb-8"></i>
                   Career
                 </li>
-                <li className="bg-[#00152e] text-sm tracking-wider cursor-pointer">
-                  <i className="fa-solid fa-chevron-right bg-[#00152e] text-xs pr-2 pb-8"></i>
+                <li className="bg-[#00152e] text-sm tracking-wider cursor-pointer w-[90px]">
+                  <i className="fa-solid fa-chevron-right bg-[#00152e] text-xs mr-2 pb-8"></i>
                   Refund
                 </li>
-                <li className="bg-[#00152e] text-sm tracking-wider cursor-pointer">
-                  <i className="fa-solid fa-chevron-right bg-[#00152e] text-xs pr-2 pb-8"></i>
+                <li className="bg-[#00152e] text-sm tracking-wider cursor-pointer w-[90px]">
+                  <i className="fa-solid fa-chevron-right bg-[#00152e] text-xs mr-2 pb-8"></i>
                   Terms
                 </li>
               </ul>
             </div>
             <div className="bg-[#00152e]">
               <ul className="list-none">
-                <li className="bg-[#00152e] text-sm tracking-wider cursor-pointer">
-                  <i className="fa-solid fa-chevron-right bg-[#00152e] text-xs pr-2 pb-8"></i>
+                <li className="bg-[#00152e] text-sm tracking-wider cursor-pointer w-[90px]">
+                  <i className="fa-solid fa-chevron-right bg-[#00152e] text-xs mr-2 pb-8"></i>
                   Details
                 </li>
-                <li className="bg-[#00152e] text-sm tracking-wider cursor-pointer">
-                  <i className="fa-solid fa-chevron-right bg-[#00152e] text-xs pr-2 pb-8"></i>
+                <li className="bg-[#00152e] text-sm tracking-wider cursor-pointer w-[90px]">
+                  <i className="fa-solid fa-chevron-right bg-[#00152e] text-xs mr-2 pb-8"></i>
                   Contact
                 </li>
-                <li className="bg-[#00152e] text-sm tracking-wider cursor-pointer">
-                  <i className="fa-solid fa-chevron-right bg-[#00152e] text-xs pr-2 pb-8"></i>
+                <li className="bg-[#00152e] text-sm tracking-wider cursor-pointer w-[90px]">
+                  <i className="fa-solid fa-chevron-right bg-[#00152e] text-xs mr-2 pb-8"></i>
                   Business
                 </li>
-                <li className="bg-[#00152e] text-sm tracking-wider cursor-pointer">
-                  <i className="fa-solid fa-chevron-right bg-[#00152e] text-xs pr-2 pb-8"></i>
+                <li className="bg-[#00152e] text-sm tracking-wider cursor-pointer w-[90px]">
+                  <i className="fa-solid fa-chevron-right bg-[#00152e] text-xs mr-2 pb-8"></i>
                   Affiliate
                 </li>
               </ul>
             </div>
           </div>
         </div>
-        <div className="bg-[#00152e] w-[32%]">
+        <div className="bg-[#00152e] lg:w-[32%]">
           <h4 className="bg-[#00152e] text-4xl font-extrabold pb-8">
             Working Hours
           </h4>
@@ -99,7 +99,7 @@ const Footer = () => {
             IT Service has been a reliable partner in the field of logistics and
             cargo forwarding.
           </p>
-          <i className="fa-solid fa-chevron-right bg-[#00152e] text-[10px] pr-2 pb-8 pt-5 text-[#0c59db]"></i>
+          <i className="fa-solid fa-chevron-right bg-[#00152e] text-[10px] mr-2 pb-8 pt-5 text-[#0c59db]"></i>
           <span className="bg-[#00152e]  text-sm text-[#0c59db] font-extrabold tracking-wider cursor-pointer">
             Discover More
           </span>
